c7c832acf107fe1549a8ae367d86e78400825b1a
Wonjin <sohanzz@naver.com>
2020-01-20 14:15:23 +0900

    Initial commit

---
+++ src/main/java/com/gabia/project/internproject/InternProjectApplication.java
@@ -0,0 +1,13 @@
+package com.gabia.project.internproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabia/project/internproject/InternProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabia.project.internproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
